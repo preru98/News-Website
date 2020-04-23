ad3df8a6c3c48a7b7513043359447f3baea929fc
Prerna <prerna1998.ps@gmail.com>
2020-04-23 23:23:06 +0530

    Updated URL

--- newsWebsite.js
+++ newsWebsite.js
@@ -4,7 +4,7 @@ for (let i = 0; i < allDivs.length; i++)
         allDivs[i].parentNode.removeChild(allDivs[i]);
     }; 
 const urlPart1='https://newsapi.org/v2/everything?q=';
-const urlPart2='&from=2019-09-05&sortBy=publishedAt&apiKey=9096f7519ec24e7d8ef475dffb1b3b61';
+const urlPart2='&from=2020-03-23&sortBy=publishedAt&apiKey=9096f7519ec24e7d8ef475dffb1b3b61';
 let buttonVar=document.getElementById('submit');
 buttonVar.addEventListener('click',function(){
     getKeyword();
